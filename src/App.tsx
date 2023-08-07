3c3baec160e44d4843a2206848a520afa2703e31
ycs4717 <123321032+ycs4717@users.noreply.github.com>
2023-08-07 23:38:17 +0900

    사이드바 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
