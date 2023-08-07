ddb4c17ac0a1a401878217144b370a7ccf4dd9fa
ycs4717 <123321032+ycs4717@users.noreply.github.com>
2023-08-07 23:41:16 +0900

    수정

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./Sidebar.scss";
 
 const Sidebar = () => {
-  return;
-  <div className="sidebar">
-    <div className="sidevarLeft"></div>
-    <div className="sidevarRight"></div>
-  </div>;
+  return (
+    <div className="sidebar">
+      <div className="sidevarLeft"></div>
+      <div className="sidevarRight"></div>
+    </div>
+  );
 };
 
 export default Sidebar;
