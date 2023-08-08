a49e2cb030d6ad11c709444a9bbb34cfd301db85
ycs4717 <123321032+ycs4717@users.noreply.github.com>
2023-08-08 21:12:19 +0900

    디자인 추가

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import "./Sidebar.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidevarLeft"></div>
-      <div className="sidevarRight"></div>
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discrord</h3>
+          <ExpandMoreIcon />
+        </div>
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHeader">
+            <div className="sidebarHeader">
+              <ExpandMoreIcon />
+              <h4>プログラミング</h4>
+            </div>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
