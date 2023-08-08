12a782a4b5b943c84fc93c5f7f958b53d6b02865
ycs4717 <123321032+ycs4717@users.noreply.github.com>
2023-08-08 22:34:57 +0900

    footer

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar = () => {
   return (
@@ -27,6 +31,27 @@ const Sidebar = () => {
               <h4>プログラミング</h4>
             </div>
             <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="./icon.png" alt="" />
+              <div className="accountName">
+                <h4>ユーザー名</h4>
+                <span>#8162</span>
+              </div>
+            </div>
+
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadsetMicIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
